73aa394376a7002b39f7e5fbe2d9dc0ba8e60ec4
Gabriel Perini <10140827900@edu.udesc.br>
2022-02-10 00:34:45 -0300

    Novas rotas para a Categoria

--- src/controllers/categoriaController.ts
+++ src/controllers/categoriaController.ts
@@ -1,8 +1,31 @@
 import {Request, Response} from 'express';
 
 import { Categoria } from '../models/Categoria'
+import { Alimento } from '../models/Alimento'
+
+
+export const getCategoria = async (req: Request, res: Response) => {
+    let { id } = req.params;
+    let categoria = await Categoria.findByPk(id);
+
+    if (categoria) {
+        res.json({ categoria })
+    } else {
+        res.json({ error: 'Categoria não encontrada' })
+    }
+}
 
 export const listCategoria = async (req: Request, res: Response) =>{
     let list = await Categoria.findAll();
+    res.json({ list });
+}
+
+export const listAlimentoFromCategoria = async (req: Request, res: Response) => {
+    let { id_categoria } = req.params;
+    console.log(id_categoria)
+
+    let list = await Alimento.findAll({
+        where: { CategoriumIdCategoria: id_categoria },
+    });
     res.json({ list });
 }
--- src/routes/categoria.ts
+++ src/routes/categoria.ts
@@ -4,6 +4,8 @@ import * as categoriaController from '../controllers/categoriaController';
 
 const categoriaRouter = Router();
 
+categoriaRouter.get('/categoria/:id', categoriaController.getCategoria)
 categoriaRouter.get('/categoria', categoriaController.listCategoria)
+categoriaRouter.get('/categoria/alimento/:id_categoria', categoriaController.listAlimentoFromCategoria);
 
 export default categoriaRouter;
